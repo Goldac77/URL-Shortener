279c5f9fc70e9aa08e2893f2544e6ddbc96a3118
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2022-11-07 18:31:54 +0000

    main section done

--- script.js
+++ script.js
@@ -2,24 +2,32 @@ const api = "https://api.shrtco.de/v2/shorten?url="
 const input = document.querySelector("#input")
 const button = document.querySelector("#button")
 const display = document.querySelector("#display")
-var displayElements = []
+var shortLink = []
+var fullLink = []
 
 button.addEventListener("click", (event) => {
+    display.innerHTML = "" //refresh the display
     event.preventDefault() //stop the page from reloading
     fetch(api + input.value)
     .then(response => response.json())
     .then(data => {
-        displayElements.push(data.result.full_short_link)
-        displayElements.map((item, index) => {
+        shortLink.push(data.result.full_short_link)
+        fullLink.push(input.value)
+        shortLink.forEach((item, index) => {
             display.innerHTML += `
-            <div id="url_${index}">
-                <p>${input.value}</p>
-                <div id="shortened">
-                    <p>${item}</p>
-                    <button>Copy</button>
+            <div class="display_content">
+                <div id="url" class="ps-5">
+                    <p><span>Full Link:</span> ${fullLink[index]}</p>
+                </div>
+
+                <div id="shortened" class="d-flex ps-5">
+                    <p><span>Shortened Link:</span> ${item}</p>
+                    <button class="btn ms-5">Copy</button>
                 </div>
             </div>
             `
         })
     })
 })
+
+console.log(shortLink)
